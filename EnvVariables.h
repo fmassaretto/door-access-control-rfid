b026193f58f1bf2410cc86c498d23c0385d2a6a6
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-02-11 03:31:08 -0300

    untrack EnvVariables.h

--- EnvVariables.h
+++
@@ -1,2 +0,0 @@
-const char MASTER_CARD_ID[18] = " YOUR MASTER RFID CARD ID HERE "; // this enable a master card to add new other cards
-const char WORKSPACE_NAME[15] = "rfid-app"; // name of the workspace for Preferencs library
