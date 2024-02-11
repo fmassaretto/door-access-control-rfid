a98ccbc99fe397c9866aa315a52553bf11c68f57
fmassaretto <fabio_no_more@yahoo.com.br>
2024-02-11 03:35:24 -0300

    Create EnvVariables_template.h

---
+++ EnvVariables_template.h
@@ -0,0 +1,3 @@
+// Rename this file to EnvVariables.h
+const char MASTER_CARD_ID[18] = " YOUR MASTER RFID CARD ID HERE "; // this enable a master card to add new other cards
+const char WORKSPACE_NAME[15] = "rfid-app"; // name of the workspace for Preferencs library
